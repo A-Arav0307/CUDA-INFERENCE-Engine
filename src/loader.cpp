431f4708253004d63e5e862b18f991d4f7b3c0e0
Aarav Gupta <25aargupta@asfg.edu.mx>
2026-06-21 16:54:55 -0500

    prototype of loader in cpp, now just need to test on main and include header file w cmake

--- src/loader.cpp
+++ src/loader.cpp
@@ -1,5 +1,6 @@
 //#include "loader.h"
 #include <iostream>
+#include <vector> 
 #include <fstream>
 #include <stdexcept> 
 
@@ -13,12 +14,18 @@ std::vector<double> load_weights(const std::string& path, size_t n) {
         throw std::runtime_error("Failed to open weights file: " + path); 
     }
     std::vector<double> final_list(n, 0.0);
-    size_t total_bytes = n * sizeof(double);
+    std::vector<float> buf(n, 0.0f); 
+    size_t total_bytes = n * sizeof(float);
+    
 
-
-    if (!file.read(reinterpret_cast<char*>(final_list.data()), total_bytes)) {
+    if (!file.read(reinterpret_cast<char*>(buf.data()), total_bytes)) {
         throw std::runtime_error("Failed to read the requested number of bytes.");
     }
+
+    for (size_t i=0; i < n; ++i){
+        final_list[i] = buf[i]; 
+    }
+
 
     return final_list; 
 }   
